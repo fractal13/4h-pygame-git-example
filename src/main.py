c036a3c1d911f5f07632e53d497975987160bdf0
BaileeAllen <Baileekallen@gmail.com>
2015-06-15 10:42:27 -0600

    fixed dumbledore placement

--- src/main.py
+++ src/main.py
@@ -65,7 +65,7 @@ class Demo(PygameGame):
 
         self.drawTextLeft(surface, "Bilbo", (255, 0, 255), 300, 30, self.font)
 
-        self.drawTextLeft(surface, "Albus Percival Wulfric Brian Dumbledore", (64, 0, 230), 240, 50, self.font)
+        self.drawTextLeft(surface, "Albus Percival Wulfric Brian Dumbledore", (64, 0, 230), 240, 130, self.font)
 
 
 
