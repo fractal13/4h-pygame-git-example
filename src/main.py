9f3845e7f51247ca2d28dc16391d4f0087acaf9f
Carter Goff <carter.goff@live.com>
2015-06-15 10:10:17 -0600

    addedNewCharacter

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ class Demo(PygameGame):
         # PygameGame sets self.width and self.height        
         PygameGame.__init__(self, "Git Demo", width_px, height_px, frames_per_second)
         pygame.font.init()
-        self.font = pygame.font.SysFont("Times New Roman",14)
+        self.font = pygame.font.SysFont("OCR A Extended",14)
         return
 
     def game_logic(self, keys, newkeys, buttons, newbuttons, mouse_position):
@@ -15,6 +15,9 @@ class Demo(PygameGame):
         # Add (don't replace) an awesome literary quote, for a different key press
         if pygame.K_b in newkeys:
             print "I am no man!"
+
+        if pygame.K_j in newkeys:
+            print "The apple had changed. Just for an instant. It had changed in mid-air."
         return
 
     def paint(self, surface):
@@ -23,7 +26,8 @@ class Demo(PygameGame):
         
         # Add (don't replace) an awesome literary character
         #                                   color            x   y
-        self.drawTextLeft(surface, "Bilbo", (255, 0, 255), 300, 30, self.font)
+        self.drawTextLeft(surface, "Bilbo", (200, 255, 0), 300, 30, self.font)
+        self.drawTextLeft(surface, "Jonas", (200, 0, 255), 300, 50, self.font)
         
         return
 
