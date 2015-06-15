47568aefb58cd9be7590a1b70b66a9ff7852d4c4
Baggins99 <17schmuave@washk12.org>
2015-06-15 10:08:35 -0600

    HGTG

--- src/main.py
+++ src/main.py
@@ -15,6 +15,8 @@ class Demo(PygameGame):
         # Add (don't replace) an awesome literary quote, for a different key press
         if pygame.K_b in newkeys:
             print "I am no man!"
+        if pygame.K_m in newkeys:
+            print "For a moment, nothing happened. Then, after a second or so, nothing continued to happen."
         return
 
     def paint(self, surface):
@@ -24,7 +26,7 @@ class Demo(PygameGame):
         # Add (don't replace) an awesome literary character
         #                                   color            x   y
         self.drawTextLeft(surface, "Bilbo", (255, 0, 255), 300, 30, self.font)
-        
+        self.drawTextLeft(surface, "Marvin", (255, 0, 255), 300, 70, self.font)
         return
 
     def drawTextLeft(self, surface, text, color, x, y, font):
