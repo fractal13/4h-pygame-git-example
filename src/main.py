651e8764dec33e0a60c3aa20ac8b60ae0728e6b7
Gilhahn <18peacokass@washk12.org>
2015-06-15 10:12:57 -0600

    moving spiderman

--- src/main.py
+++ src/main.py
@@ -8,6 +8,8 @@ class Demo(PygameGame):
         PygameGame.__init__(self, "Git Demo", width_px, height_px, frames_per_second)
         pygame.font.init()
         self.font = pygame.font.SysFont("Times New Roman",14)
+        self.x = 282
+        self.y = 30
         return
 
     def game_logic(self, keys, newkeys, buttons, newbuttons, mouse_position):
@@ -15,6 +17,21 @@ class Demo(PygameGame):
         # Add (don't replace) an awesome literary quote, for a different key press
         if pygame.K_b in newkeys:
             print "I am no man!"
+
+        if pygame.K_s in newkeys:
+            print "noooooooooooooooo"
+
+        if pygame.K_RIGHT in keys:
+            self.x += 2
+
+        if pygame.K_UP in keys:
+            self.y -= 2
+
+        if pygame.K_LEFT in keys:
+            self.x -= 2
+
+        if pygame.K_DOWN in keys:
+            self.y += 2
         return
 
     def paint(self, surface):
@@ -23,8 +40,13 @@ class Demo(PygameGame):
         
         # Add (don't replace) an awesome literary character
         #                                   color            x   y
+
         self.drawTextLeft(surface, "Bilbo", (255, 0, 255), 300, 30, self.font)
         
+        # Add (don't replace) an awesome literary character
+        #                                   color            x   y
+        self.drawTextLeft(surface, "Spider-man", (255, 255, 0), self.x, self.y, self.font)
+
         return
 
     def drawTextLeft(self, surface, text, color, x, y, font):
