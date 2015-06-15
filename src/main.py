78ab63c4e7b849209c70548b5cb1b10871284834
ItsMeSterling <shnazzyup@gmail.com>
2015-06-15 11:46:50 -0600

    Name and something else

--- src/main.py
+++ src/main.py
@@ -17,6 +17,9 @@ class Demo(PygameGame):
     def game_logic(self, keys, newkeys, buttons, newbuttons, mouse_position):
         
         # Add (don't replace) an awesome literary quote, for a different key press
+        if pygame.K_a in newkeys:
+            print "The one with ravishing hair."
+        
         if pygame.K_b in newkeys:
             print "I am no man!"
 
@@ -89,7 +92,10 @@ class Demo(PygameGame):
 
 
         self.drawTextLeft(surface, "Ender", (0, 255, 0), 30, 300, self.font)
-
+        
+        self.drawTextLeft(surface, "Bean", (0, 255, 0), 30, 400, self.font)
+        
+        
 
         
 
