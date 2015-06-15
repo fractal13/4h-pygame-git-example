d1bfc62787f54805a2666e105722c52a810b546a
mallen43 <girls@ip-144-38-185-5.dixie.edu>
2015-06-15 10:31:42 -0600

    Changes

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ class Demo(PygameGame):
         # PygameGame sets self.width and self.height        
         PygameGame.__init__(self, "Git Demo", width_px, height_px, frames_per_second)
         pygame.font.init()
-        self.font = pygame.font.SysFont("Times New Roman",14)
+        self.font = pygame.font.SysFont("OCR A Extended",14)
         return
 
     def game_logic(self, keys, newkeys, buttons, newbuttons, mouse_position):
@@ -16,20 +16,8 @@ class Demo(PygameGame):
         if pygame.K_b in newkeys:
             print "I am no man!"
 
-
-
-        if pygame.K_m in newkeys:
-            print "For a moment, nothing happened. Then, after a second or so, nothing continued to happen."
-
-
         if pygame.K_g in newkeys:
             print "You shall not pass!"
-
-
-        if pygame.K_m in newkeys:
-            print "Does not Happen"
-
-
 
         if pygame.K_j in newkeys:
             print "The apple had changed. Just for an instant. It had changed in mid-air."
@@ -37,7 +25,6 @@ class Demo(PygameGame):
         if pygame.K_m in newkeys:
             print "Does not Happen"
     
-
         if pygame.K_c in newkeys:
             print "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn"
 
@@ -70,11 +57,13 @@ class Demo(PygameGame):
         self.drawTextLeft(surface, "Mau", (255, 0, 255), 300, 90, self.font)
 
 
+<<<<<<< Updated upstream
         self.drawTextLeft(surface, "Marvin", (255, 0, 255), 300, 70, self.font)
 
 
+=======
+>>>>>>> Stashed changes
         self.drawTextLeft(surface, "Gandalf", (34, 139, 34), 300, 120, self.font)
-
 
 
         self.drawTextLeft(surface, "Ender", (0, 255, 0), 30, 300, self.font)
